94f3b34d76d0f1ac7b9c703f01b46e7ac774a27c
Daniel Crowley <dan@relectrify.com>
2021-03-06 16:20:12 +1100

    UTF8 Error

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,7 @@
 
-import React, { Component } from "react";
-import _ from 'lodash/array'; 
-
+import _ from 'lodash/array';
+import React from "react";
+import { pushWaveformData } from '../services/firebase';
 
 export default function Home() {
 
@@ -13,53 +13,62 @@ export default function Home() {
   var data_field = "" 
   var MessageReceived=false
   
-  var websock = new WebSocket("ws://192.168.8.138/tcp_proxy")
+  var websock = new WebSocket("ws://192.168.8.155/tcp_proxy")
   websock.binaryType = 'arraybuffer';
   websock.onopen = function (event){
     websock.send("*IDN?\n")
     data_field="id"
   }
   websock.onmessage = (message)=>{
-    
     if (data_type === 'Snap') {
       metadata[data_field]=message.data
       MessageReceived=true}
-    if (data_type === 'Channel'){
+    else if (data_type === 'Channel'){
       channeldata[data_field]=message.data
       MessageReceived=true}
-    if (data_type === 'Waveform'){
-      var waveformPacket = message.data.split(',')
-      console.log(waveformPacket)
-      waveformPacket.shift()
-      var lastelement=waveformPacket.pop()
-      waveformData.push(...waveformPacket)
-      console.log("waveform saving..")
-      
-      if (lastelement==="\n") {
-        console.log(waveformData)
+    else if (data_type === 'Waveform'){
+
+      var waveformPacket = message.data.split(',') //split string to array
+      waveformPacket.shift()  //remove gumpf at start of array
+      var lastelement=waveformPacket.pop() //remove gumpf at start of array, 
+   
+      waveformData.push(...waveformPacket) //push clean packet into array    
+      if (lastelement==="\n") { //if new line, it's the last packet and the full message has been received
         MessageReceived=true
       }
     }
-    if (data_type === '') {
+    else if (data_type === 'Screenshot'){
       console.log(message.data)
+    }
+    else {
+      console.log("Empty: "+ message.data)
       MessageReceived=true}
-
-  }
-  async function wait(ms){
-    await new Promise(r => setTimeout(r, ms))
   }
 
-  async function sendWithDelay(command,name,dataType,delay=50){
-    MessageReceived=false
+  async function sendWithDelay(command,name,dataType,expectResponse=true){
+    expectResponse? MessageReceived = false : MessageReceived = true
+    var delay=50
     data_type=dataType
     data_field = name
+
     websock.send(command)
-    await new Promise(r => setTimeout(r, delay))
+
+    await new Promise(r => {
+      var check=()=>{
+        if (MessageReceived) r()
+        delay=delay*5
+        setTimeout(check, delay)
+      }
+      setTimeout(check, delay)
+
+    })
   }
-
-
-      
    async function saveWaformData( Mode="screen"){
+    metadata = {}
+    channeldata = {}
+    waveformData = []
+    waveformDataAll = []
+    
      console.log("saving..")
       await sendWithDelay(":TIM:SCAL?\n","timeScale","Snap")
       await sendWithDelay(`:ACQ:SRAT? Channel1\n`,"SampleRate","Snap") 
@@ -71,45 +80,41 @@ export default function Home() {
       await sendWithDelay(`:${Channels[i]}:OFFS?\n`,"Offset","Channel")
       await sendWithDelay(`:${Channels[i]}:UNIT?\n`,"Unit","Channel")
       metadata['Channels'].push(channeldata)
-      Mode="All"
-      await sendWithDelay(`:waveform:source ${Channels[i]}\n`,"","")
+      Mode="Screen"
+      await sendWithDelay(`:waveform:source ${Channels[i]}\n`,"","",false)
       switch(Mode) {
         case "Screen":
-          await sendWithDelay(`:WAV:MODE Normal\n`,"","")
+          await sendWithDelay(`:WAV:MODE Normal\n`,"","",false)
           break
         case "All":
-          await sendWithDelay(`:WAV:MODE Raw\n`,"","")
-          await sendWithDelay(`:WAV:POIN 1000\n`,"","")
+          await sendWithDelay(`:WAV:MODE Raw\n`,"","",false)
+          await sendWithDelay(`:WAV:POIN 10000000\n`,"","",false)
           break
         case "Test":
-          await sendWithDelay(`:WAV:MODE Raw\n`,"","")
-          await sendWithDelay(`:WAV:POIN 100\n`,"","")
-
+          await sendWithDelay(`:WAV:MODE Raw\n`,"","",false)
+          await sendWithDelay(`:WAV:POIN 100\n`,"","",false)
           break
         default:
-          await sendWithDelay(`:WAV:MODE Normal\n`,"","")
+          await sendWithDelay(`:WAV:MODE Normal\n`,"","",false)
           break
       }
-      await sendWithDelay(`:WAV:MODE ASCii\n`,"","")
-      await sendWithDelay(`:waveform:data?\n`,"","Waveform",6000)
+      await sendWithDelay(`:WAV:FORM ASCii\n`,"","",false)
+      await sendWithDelay(":waveform:data?\n","","Waveform")
       channeldata={}
       waveformDataAll.push(waveformData)
       waveformData = []
+      await sendWithDelay(":DISP:DATA?\n","","Screenshot")
+      
     }   
+    metadata.DataPoints=waveformDataAll[0].length
+    var timestamps= Array.from({length: metadata.DataPoints}, (_, i) => i)
+    waveformDataAll.unshift(timestamps)
     var csvwaveform = _.zip(...waveformDataAll)
-    console.log(csvwaveform)
     
-    // timestamps=range(1,int(metadata['DataPoints']) )
-    // waveformdata.insert(0,timestamps)
-
     // bmp_bin = inst.query_binary_values(':DISP:DATA?', datatype='B', container=bytes)
     // img = Image.open(io.BytesIO(bmp_bin))
     // img.save(output, output.split('.')[-1])
-    
-    // print (metadata)
-    
-    // csvwaveform = zip(*waveformdata)
-    
+      
     // with open('some.csv', 'w', newline='') as f:
     //     writer = csv.writer(f)
     //     writer.writerows(csvwaveform)
@@ -118,9 +123,13 @@ export default function Home() {
       console.log(metadata)
 
       console.log("waveformDataAll")
-      console.log(waveformDataAll)
-      console.log(waveformDataAll.length)
+      console.log(csvwaveform)
+      let csvContent = "data:text/csv;charset=utf-8," 
+      + csvwaveform.map(e => e.join(",")).join("\n");
 
+      pushWaveformData(csvContent,"ID1")
+      var encodedUri = encodeURI(csvContent);
+      window.open(encodedUri);
     }
     
     return (
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,15 +1,54 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyDj_-XAfpibzPIb6DLwkj9Hz1aGFaTiH1s",
-    authDomain: "online-oscilloscope.firebaseapp.com",
-    databaseURL: "https://online-oscilloscope.firebaseio.com",
-    projectId: "online-oscilloscope",
-    storageBucket: "online-oscilloscope.appspot.com",
-    messagingSenderId: "525427984671",
-    appId: "1:525427984671:web:ddb13362e939123215c82a",
-    measurementId: "G-0P1VV17036"
-  };
-  firebase.initializeApp(config);
-  export const auth = firebase.auth;
-  export const db = firebase.database();
+  apiKey: "AIzaSyDj_-XAfpibzPIb6DLwkj9Hz1aGFaTiH1s",
+  authDomain: "online-oscilloscope.firebaseapp.com",
+  databaseURL: "https://online-oscilloscope.firebaseio.com",
+  projectId: "online-oscilloscope",
+  storageBucket: "online-oscilloscope.appspot.com",
+  messagingSenderId: "525427984671",
+  appId: "1:525427984671:web:ddb13362e939123215c82a",
+  measurementId: "G-0P1VV17036"
+};
+firebase.initializeApp(config)
+//export default !firebase.apps.length ? firebase.initializeApp(config) : firebase.app()
+export const auth = firebase.auth;
+export const db = firebase.database();
+const storage = firebase.storage();
+
+
+// export default class fb_s{
+//   constructor(name) {
+//     this.name = name;
+//   }
+//   function getMetaData(key){}
+//   function pushMetaData(data){}
+ export function pushWaveformData(data,id){
+  var WavformRef = storage.ref(`CustomerID/Wavform/${id}`);
+
+  WavformRef.putString(data).then((snapshot) => {
+    console.log('Uploaded a raw string!');
+  });
+}
+  
+// }
+// }
+
+// class Singleton {
+//   constructor() {
+//     firebase.initializeApp(config) 
+//    // ...
+//   }
+
+//   method1() {
+//     // ...
+//   }
+
+//   method2() {
+//     // ...
+//   }
+// }
+
+// const fb = new Singleton();
+
+// Object.freeze(singletonInstance)
